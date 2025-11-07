9ed429f679e2441851c91c5627056077bc5b5668
MichaelP <michaelp@cytechint.com>
2025-11-07 13:31:59 +0800

    feat: add knights tour main func

--- ProgramLoader.java
+++ ProgramLoader.java
@@ -18,6 +18,7 @@ public class ProgramLoader {
         programs.add(new programs.fibonacci.FibonacciProgram());
         programs.add(new programs.hanoi.HanoiProgram());
         programs.add(new programs.palindrome.PalindromeProgram());
+        programs.add(new programs.knightstour.KnightsTourProgram());
 
         // Example: programs.add(new programs.[folder].[ClassName]());
         // Only one line per new program.
